7a20f7413b9d5fa9c3fbea67a86367acdbb739e9
Rangarajan-97 <59253428+Rangarajan-97@users.noreply.github.com>
2021-08-24 14:04:39 +0530

    Add files via upload

--- LoginPage.java
+++ LoginPage.java
@@ -4,7 +4,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -16,42 +18,53 @@ public class LoginPage {
 	 String password = "Test123!";
 	 
 	
-    @BeforeClass //This method is to navigate flipkart URL
+    @BeforeClass //This method is to navigate High level Login URL
 	public void Open_Browser(){
 		 System.setProperty("webdriver.chrome.driver", "//Users//ranga//Downloads//chromedriver");
 		 wd = new ChromeDriver();
 		 wd.manage().window().maximize();
 		 wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
          wd.get("https://app.gohighlevel.com/");
-        String Title= wd.getTitle();
-        System.out.println(Title);
-		}
+  		}
 	
 	
-	@Test // To login go high level
-	public void Login_page() {
+	
+    @Test // To login go high level
+	public void Login_page() throws InterruptedException {
 		 wd.findElement(By.xpath("//input[@id='email']")).sendKeys(username);
 		 wd.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
 		 wd.findElement(By.xpath("//button[contains(text(),'Sign in')]")).click();
-		 
-		 
-	}
-	
-	@Test() //Navigating to settings Page
-	public void Setting_Page() throws InterruptedException {
-		 wd.findElement(By.xpath("//span[contains(text(),'Settings')]")).click();
-		 wd.findElement(By.xpath("//li/a[contains(text(),'Calendars')]")).click();
 		 Thread.sleep(5);
+}
+	
+	
+	
+    @Test() //Navigating to settings Page
+	public void Setting_Page() throws InterruptedException {
+		 WebElement element =wd.findElement(By.xpath("//span[contains(text(),'Settings')]"));
+		 
+		 Actions action = new Actions(wd);
+		 action.moveToElement(element);
+		 action.click().build().perform();
+		 
+		 wd.findElement(By.xpath("//li/a[contains(text(),'Calendars')]")).click();
+		 
   }
 	
-	@Test()
+	
+    @Test()
 	public void CreateNewCalendar() throws InterruptedException {
-		wd.findElement(By.xpath("//span[contains(text(),'calendar_test')]/following::span[contains(text(),'New Calendar')]")).click();
-		 Thread.sleep(10);
+    	
+    	WebElement ele = wd.findElement(By.xpath("//span[contains(text(),'calendar_test')]/following::span[contains(text(),'New Calendar')]"));
+		Actions action = new Actions(wd);
+		action.moveToElement(ele);
+		action.click().build().perform();
+		Thread.sleep(10);
 
 	}
 
-	//@AfterClass
+	
+    //@AfterClass
 	//public void quit() {
 	//	wd.close();
 	//}
